00e1aabf2aa1d598883a22cd212e6005841ee18c
MrVictor42 <guin-hu@hotmail.com>
2019-08-31 11:55:57 -0300

    Delete user

--- users/api/views.py
+++ users/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.generics import RetrieveAPIView, UpdateAPIView
+from rest_framework.generics import RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
 from users.models import User
 from .serializers import UserSerializer
@@ -15,6 +15,11 @@ class UserDetail(RetrieveAPIView):
     serializer_class = UserSerializer
 
 class UserUpdate(UpdateAPIView):
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDelete(DestroyAPIView):
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
